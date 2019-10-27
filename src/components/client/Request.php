c53fdc4466e54f5c944804f1db1ff369f8017634
Roman Kulikov <r.v.kulikov@yandex.ru>
2019-10-27 03:04:05 +0300

    request

--- src/components/client/Request.php
+++ src/components/client/Request.php
@@ -1,7 +1,26 @@
 <?php
 namespace rvkulikov\amo\module\components\client;
 
+use yii\httpclient\Exception;
+use yii\httpclient\Response;
+
+/**
+ *
+ */
 class Request extends \yii\httpclient\Request
 {
+    /**
+     * @var Client owner client instance.
+     */
+    public $client;
 
+    /**
+     * @return Response
+     * @throws Exception
+     */
+    public function send()
+    {
+        $this->addHeaders(['Authorization' => "Bearer {$this->client->accessToken}"]);
+        return parent::send();
+    }
 }
