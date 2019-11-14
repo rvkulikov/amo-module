5ad1cbd7da81a1f28bac3aa2a775b8be5ea10e37
Roman Kulikov <r.v.kulikov@yandex.ru>
2019-11-14 21:46:49 +0300

    Job WIP

--- src/jobs/SyncAccount_Job.php
+++ src/jobs/SyncAccount_Job.php
@@ -1,8 +1,12 @@
 <?php
-
 namespace rvkulikov\amo\module\jobs;
 
+use rvkulikov\amo\module\exceptions\InvalidModelException;
+use rvkulikov\amo\module\services\account\sync\AccountSyncer_Cfg;
+use rvkulikov\amo\module\services\account\sync\AccountSyncer_Interface;
 use yii\base\BaseObject;
+use yii\base\InvalidConfigException;
+use yii\httpclient\Exception;
 use yii\queue\JobInterface;
 use yii\queue\Queue;
 
@@ -11,13 +15,29 @@ use yii\queue\Queue;
  */
 class SyncAccount_Job extends BaseObject implements JobInterface
 {
+    /** @var array|AccountSyncer_Cfg */
+    public $cfg;
+    /** @var AccountSyncer_Interface */
+    private $accountSyncer;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct(AccountSyncer_Interface $accountSyncer, $config = [])
+    {
+        parent::__construct($config);
+        $this->accountSyncer = $accountSyncer;
+    }
 
     /**
      * @param Queue $queue which pushed and is handling the job
      * @return void|mixed result of the job execution
+     * @throws InvalidModelException
+     * @throws InvalidConfigException
+     * @throws Exception
      */
     public function execute($queue)
     {
-        // TODO: Implement execute() method.
+        $this->accountSyncer->sync($this->cfg);
     }
 }
